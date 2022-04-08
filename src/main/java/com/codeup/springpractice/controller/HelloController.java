d8ee055ace2cc11301a48878f6be14bd129ecbcd
Alfonso Carrillo <alfonso.ivan.carrillo@gmail.com>
2022-04-08 00:01:59 -0500

    example of hello exercise

--- src/main/java/com/codeup/springpractice/controller/HelloController.java
+++ src/main/java/com/codeup/springpractice/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.codeup.springpractice.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,10 +16,16 @@ public class HelloController {
         return "<h1>Hello from Spring!</h1><br><h1>Spring Practice!</h1>";
     }
 
+//    @GetMapping("/hello/{name}")
+//    @ResponseBody
+//    public String sayHello(@PathVariable String name){
+//        return "<h1>Hello " + name + "!</h1>";
+//    }
+
     @GetMapping("/hello/{name}")
-    @ResponseBody
-    public String sayHello(@PathVariable String name){
-        return "<h1>Hello " + name + "!</h1>";
+    public String sayHello(@PathVariable String name, Model model){
+        model.addAttribute("name", name);
+        return "hello";
     }
 
     @GetMapping( "/increment/{number}")
